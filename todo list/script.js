b02b85387081605d42d3ed7f079f92b5269a732d
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2025-06-08 07:34:31 +0530

    Create script.js

---
+++ todo list/script.js
@@ -0,0 +1,15 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter a task.");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.innerHTML = `${taskText} <span class="delete" onclick="this.parentElement.remove()">X</span>`;
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
